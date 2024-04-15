1bf7b5b01fae99e4642be8bc68da4e8c810983c0
tsutomu-nagano <copyxshow@gmail.com>
2024-04-15 19:27:47 +0900

    dockerfileでのパスを修正

--- excel2json/main.py
+++ excel2json/main.py
@@ -25,7 +25,7 @@ from openpyxl import load_workbook
 
 from pandas import json_normalize
 
-from . import excle2json_core
+from excel2json import excel2json
 
 tags_metadata = [
     {
@@ -70,7 +70,7 @@ def excel_to_json(
         # これ実行しないとなぜか戻り値がNoneになる・・・
         y = yaml.safe_load(t2)
 
-        item = excle2json_core.xls_with_yaml2json(src = t1.name, config = t2.name)
+        item = excel2json.xls_with_yaml2json(src = t1.name, config = t2.name)
         json_compatible_item_data = jsonable_encoder(item)
         return JSONResponse(content=json_compatible_item_data)
 
